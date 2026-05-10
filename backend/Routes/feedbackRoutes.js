1947393f21875070ee67aeb27c17c24cfacc5bbe
Eshal Asad <eshalasad19@gmail.com>
2026-05-10 21:39:12 +0500

    file update

--- backend/Routes/feedbackRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitFeedback, getAllFeedbacks } = require('../controllers/feedbackController');
-const { protect, adminOnly } = require('../middleware/authMiddleware');
-
-router.post('/', protect, submitFeedback);
-router.get('/', protect, adminOnly, getAllFeedbacks);
-
-module.exports = router;
